0bb63f935ed1e53090b26014191c8af7c1acdbe6
aatu.inkilainen@seamk.fi <aatu.inkilainen@seamk.fi>
2022-09-19 09:04:10 +0300

    GIT COMMIT

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 // Ota mongoose käyttöön -> tietokantayhteys
 const mongoose = require('mongoose');
 
+//Ota books  muista vaihtaa harkassa oikea tiedoston nimi
+const book = require('./bookSchema.js');
+
 //Ota mongodb käyttöön -- palataan asiaan, tarviiko asentaa erikseen
 const mongodb = require('mongodb');
 
@@ -17,6 +20,19 @@ const { userInfo } = require('os');
 app.use(express.static('public'));
     //Ota käyttöön bodyparser
  app.use(bodyparser.urlencoded({extended:false}));
+
+//Muodostetaan tietokanta yhteys
+// Luo vakio connecrionstringille
+const uri = 'mongodb+srv://AATUpro:<password>@cluster0.pusklqr.mongodb.net/?retryWrites=true&w=majority'
+//Muodostetaan yhteys tietokantaan
+mongoose.connect(uri, { useUnifiedTopology: true, UseNewUrlParser:true})
+
+//Luodaan vakio tietokantayhteydelle
+const db = mongoose.connection
+//Näytä ilmoitus, jos yhteys ok
+db.once('open', function() {
+    console.log('Tietokantayhteys avattu');
+})
 
 //Laitetaan palvelin kuuntelemaan porttia 8080
 const server = app.listen(8080, function(){});
